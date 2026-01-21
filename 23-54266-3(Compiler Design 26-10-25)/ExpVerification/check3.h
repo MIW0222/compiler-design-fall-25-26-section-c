10edd9f77a7fbf32c7501ad0312adb3c843fc971
MIW0222 <barome2000@gmail.com>
2026-01-22 03:26:27 +0600

    Add files via upload

---
+++ 23-54266-3(Compiler Design 26-10-25)/ExpVerification/check3.h
@@ -0,0 +1,8 @@
+#include<iostream>
+#include <string>
+
+using namespace std;
+
+bool checkStr3(string in);
+
+int check3A(string in);
---
+++ 23-54266-3(Compiler Design 26-10-25)/ExpVerification/check4.h
@@ -0,0 +1,8 @@
+#include<iostream>
+#include <string>
+
+using namespace std;
+
+bool checkStr4(string in);
+
+int check4A(string in);
